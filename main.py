f194152bb3b4d793c30571b8fd7a62f136d43d91
Julia Braccini <jubraccini2@gmail.com>
2024-07-03 21:07:09 -0300

    Versão inicial do projeto com o script main.py

--- main.py
+++ main.py
@@ -1,16 +1,41 @@
-from funcoes import limparTela, aguardar
+import os
+import time
+
+def limparTela():
+    # Windows
+    if os.name == 'nt':
+        os.system('cls')
+    # Mac e Linux (posix)
+    else:
+        os.system('clear')
+
+def aguardar(segundos):
+    time.sleep(segundos)
 
 while True:
     limparTela()
     print("(0) Sair")
     print("(1) Incluir Aluno")
     print("(2) Mostrar Lista")
-    print("(3) de")
     opcao = input()
     if opcao == "0":
         break
     elif opcao == "1":
-        nome = input("Nome:")
-        email = input("E-mail:")
-        arquivo = open("bd.atitus", "w") #w write #r read #a append
-        arquivo.write(nome+" "+email+")
+        nome = input("Nome: ")
+        email = input("E-mail: ")
+        with open("bd.atitus", "a") as arquivo:  # Usa modo append para adicionar ao arquivo sem sobrescrever
+            arquivo.write(f"{nome} {email}\n")
+        aguardar(2)
+    elif opcao == "2":
+        limparTela()
+        print("Lista de Alunos:")
+        try:
+            with open("bd.atitus", "r") as arquivo:
+                for linha in arquivo:
+                    print(linha.strip())
+        except FileNotFoundError:
+            print("Nenhum aluno encontrado. Adicione alunos primeiro.")
+        aguardar(5)
+    else:
+        print("Opção inválida. Tente novamente.")
+        aguardar(2)
